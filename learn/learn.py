72dcd491988b3f483b627a0a98de3ddccaeb1679
JClokey <joeclokey@gmail.com>
2022-12-28 08:28:17 +1000

    Update learn.py
    Beginning input of dictionary for learning

--- learn/learn.py
+++ learn/learn.py
@@ -2,3 +2,20 @@
 This will contain functions for teaching, learning and general glossaries for passive sampling
 """
 
+terms = {}
+
+def kinetic_sampling():
+    print("Kinetic mode sampling occurs in the first phase of uptake when ")
+
+def equilibrium_sampling():
+    print("Equilibrium sampling occurs when the passive sampler is exposed for long enough to reach thermodynamic equilibrium")
+
+def define(term):
+    #This will print an explanation of the term input
+    pass
+
+def list_terms():
+    #This will be used to print a list of optional terms which can be defined
+    print(terms.keys())
+    pass
+
