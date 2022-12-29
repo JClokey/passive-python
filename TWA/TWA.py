f5c96d40768d39d1a053be15e8a6f8eb01035f3b
JClokey <joeclokey@gmail.com>
2022-12-30 08:07:47 +1000

    Updated time weighted averages
    Changed name of module, added some time weighted average maths and a function for such

---
+++ TWA/TWA.py
@@ -0,0 +1,28 @@
+"""
+One of the benefits of using passive samplers in the kinetic or non-linear (as opposed to equilibrium) phases is that they represent the concentration over the period of deployment
+This allows us to back calculate a time weighted average concentration of an analyte for the period of deployment
+
+"""
+
+"""
+TO BE REMOVED
+General notes
+Cs = Cw*Ksw(1-e^[-Ke*t])
+where Cs is the concentration (μg g−1) of the analyte in the sorbent, 
+Cw the TWA concentration (μg l−1) of the analyte in water, 
+Ksw the POCIS-water partition constant (l g−1) and ke the elimination rate constant (d−1).
+
+
+If the elimination rate ke is negligible compared to the uptake rate Ku (l g−1 d−1 or ml g−1 d−1), then the sampler acts as an infinite sink and we can redue to:
+Cs = Cw*Ku*t
+
+If we introduce the mass of the sorbent (Ms) we can change out Ku for a relationship with Rs (sampling rate)
+Cs = (Cw*Rs*t)/Ms
+
+Thus we can solve for Cw which is the time weighted average by rearranging to:
+Cw = (Cs*Ms)/(Rs*t)
+"""
+
+def time_weighted_average(sampling_rate, time, mass, conc_sorbent):
+    TWA = (conc_sorbent * mass)/(sampling_rate * time)
+    return TWA
